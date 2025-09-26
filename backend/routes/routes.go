4869a3a7dc01333ef95f5853eef3f0e2d0920c46
iki <rifkiqizara23@gmail.com>
2025-09-26 15:30:11 +0700

    feat: add cors configuration to be for localhost:5173 (development)

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,12 +1,25 @@
 package routes
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/rifchzschki/Audio-Steganografi/backend/controllers"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://fe-audio-steg.vercell.app"}, 
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour, 
+	}
+
+	router.Use(cors.New(config))
 
 	api := router.Group("/api")
 	{
